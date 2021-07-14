5621a9b47cac966aef1d531635a355e8c79c3fc5
birdwatcherYT <aabcehmu.362880@hotmail.co.jp>
2021-07-14 19:07:39 +0900

    DQNを修正

--- Deep_Q_learning.py
+++ Deep_Q_learning.py
@@ -4,6 +4,7 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
+from keras.layers.advanced_activations import PReLU
 from collections import deque
 
 
@@ -24,10 +25,12 @@ class Memory:
 
 def DQN(loss, input_dim, output_dim, hidden_size, learning_rate):
     model = Sequential()
-    model.add(Dense(hidden_size, activation="relu", input_dim=input_dim))
-    model.add(Dense(hidden_size, activation="relu"))
-    model.add(Dense(output_dim, activation="linear"))
-    model.compile(loss=loss, optimizer=Adam(lr=learning_rate))
+    model.add(Dense(hidden_size, input_dim=input_dim))
+    model.add(PReLU())
+    model.add(Dense(hidden_size))
+    model.add(PReLU())
+    model.add(Dense(output_dim))
+    model.compile(optimizer=Adam(learning_rate), loss=loss)
     return model
 
 
@@ -49,41 +52,34 @@ class Deep_Q_learning:
         self.Qtarget = DQN(
             loss, self.state_dim, self.num_action, hidden_size, learning_rate
         )
-        # self.Qmodel = Sequential()
-        # self.Qmodel.add(Dense(hidden_size, activation="relu", input_dim=self.state_dim))
-        # self.Qmodel.add(Dense(hidden_size, activation="relu"))
-        # self.Qmodel.add(Dense(self.num_action, activation="linear"))
-        # self.Qmodel.compile(loss=loss, optimizer=Adam(lr=learning_rate))
         # メモリ
         self.memory = Memory(memory_size)
 
-    def update(self, batch_size, discount_rate):
+    def update(self, batch_size, discount_rate, epoch):
         if self.memory.len() < batch_size:
             return
         mini_batch = self.memory.sample(batch_size)
 
         # 状態を入力として
-        inputs = np.array([state for (state, _, _, _) in mini_batch])
+        inputs = np.array([state for (state, _, _, _, _) in mini_batch])
         # 基本はQネットワークの出力をそのまま予測する
-        # targets = self.Qmodel.predict(inputs)
         targets = self.Qmain.predict(inputs)
 
-        actions = np.array([action for (_, action, _, _) in mini_batch])
-        rewards = np.array([reward for (_, _, reward, _) in mini_batch])
+        actions = np.array([action for (_, action, _, _, _) in mini_batch])
+        rewards = np.array([reward for (_, _, reward, _, _) in mini_batch])
 
-        next_states = np.array([next_state for (_, _, _, next_state) in mini_batch])
-        # pred_max = self.Qmodel.predict(next_states).max(axis=1)
+        next_states = np.array([next_state for (_, _, _, next_state, _) in mini_batch])
+        not_fins = np.array([int(not fin) for (_, _, _, _, fin) in mini_batch])
         next_actions = self.Qmain.predict(next_states).argmax(axis=1)
-        # pred_max = self.Qtarget.predict(next_states).max(axis=1)
         pred_max = self.Qtarget.predict(next_states)[
             np.arange(batch_size), next_actions
         ]
         # 最小化したいものは E[ ( 報酬 + 割引率 * ( max_{a'} Q(s', a') ) - Q(s, a) )^2 ] なので、
         # 報酬 + 割引率 * ( max_{a'} Q(s', a') ) がターゲット
-        targets[np.arange(batch_size), actions] = rewards + discount_rate * pred_max
+        targets[np.arange(batch_size), actions] = rewards + discount_rate * pred_max * not_fins
 
-        # self.Qmodel.train_on_batch(inputs, targets)
-        self.Qmain.train_on_batch(inputs, targets)
+        # self.Qmain.train_on_batch(inputs, targets)
+        self.Qmain.fit(inputs, targets, epochs=epoch, verbose=0)
 
     def select_action(self, random_rate):
         """一定の確率で、ベストでない動きをする"""
@@ -93,42 +89,47 @@ class Deep_Q_learning:
 
     def select_best_action(self):
         """評価値の最も高い行動を探す"""
-        # pred = self.Qmodel.predict([self.state])[0]
         pred = self.Qmain.predict([self.state])[0]
-        return np.argmax(pred)
+        action = pred.argmax()
+        return action
 
     def get_state(self):
         row, col = self.maze.board_size()
         x, y = self.maze.get_position()
         return x / row, y / col
-        # return x,y
+        # return x, y
 
     def reward(self):
-        # return 1 if self.maze.is_goal() else 0
         return 1 if self.maze.is_goal() else -1
         # return 0 if self.maze.is_goal() else -1
+        # return 1 if self.maze.is_goal() else 0
 
     def from_start(self):
         self.maze.reset()
         self.state = self.get_state()
 
-    def step(self, discount_rate, random_rate, batch_size):
+    def step(self, random_rate):
         # 行動の選択。ベストアクションとは限らない。
         action = self.select_action(random_rate)
         # 選択された行動に従い動く。ただし、壁がある場合は無視される
         self.maze.move(action)
         # 移動後の状態を取得
         next_state = self.get_state()
+        # ゴールしたかどうか
+        fin = self.maze.is_goal()
         # 報酬
         reward = self.reward()
         # メモリに追加
-        self.memory.add((self.state, action, reward, next_state))
-
-        # Qネットワークの重みを更新する
-        # self.update(batch_size, discount_rate)
-
+        self.memory.add((self.state, action, reward, next_state, fin))
         # 移動後の状態を現在の状態に記録
         self.state = next_state
 
     def Qmain_to_Qtarget(self):
         self.Qtarget.set_weights(self.Qmain.get_weights())
+
+    def save_weights(self, filename):
+        self.Qmain.save_weights(filename)
+
+    def load_weights(self, filename):
+        self.Qmain.load_weights(filename)
+        self.Qmain_to_Qtarget()
--- main_dqn.py
+++ main_dqn.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # 強化学習入門 実践
-
 import time
-
 from Maze import Maze
 from Deep_Q_learning import Deep_Q_learning
 
@@ -21,69 +18,71 @@ from Deep_Q_learning import Deep_Q_learning
 #     "W                   W",
 #     "WWWWWWWWWWWWWWWWWWWWW",
 # ]
-BOARD = [
-    "WWWWWWWWWWWWWWWWWWWWW",
-    "WS    W     W       W",
-    "W W WWWWW W W W WWWWW",
-    "W W       W   W W   W",
-    "W W W WWW WWWWW W WWW",
-    "W W W       W       W",
-    "W W W WWW WWW W WWWWW",
-    "W             W    GW",
-    "WWWWWWWWWWWWWWWWWWWWW",
-]
 
+# このサイズだと160epochくらいでマシになる↓
+BOARD = [
+    "WWWWWWWWW",
+    "WSW W   W",
+    "W W W WWW",
+    "W     W W",
+    "W WWW W W",
+    "W   W   W",
+    "W W W WWW",
+    "W W W  GW",
+    "WWWWWWWWW",
+]
 # 何回ゴールするか
 EPISODE_MAX = 1000
 # ゴールまでの打ち切りステップ数
 STEP_MAX = 3000
 # 学習率
-LEARNING_RATE = 0.0001
+LEARNING_RATE = 0.001
 # 割引率
-DISCOUNT_RATE = 0.95
+DISCOUNT_RATE = 0.9
 # 描画スピード
 SLEEP_TIME = 0.0001
 
 # 隠れ層のサイズ
 HIDDEN_SIZE = 32
 # 蓄える行動履歴データの数
-MEMORY_SIZE = 10000
+MEMORY_SIZE = 500000
 # バッチサイズ
-BATCH_SIZE = 32
+BATCH_SIZE = 128
 # 損失関数
-LOSS = "huber_loss"
-
+LOSS = "mse"
+# LOSS = "huber_loss"
+# ミニバッチで繰り返す学習回数
+EPOCH = 1
+# 更新する間隔
+UPDATE_INTERVAL = 10
+# ターゲットネットワークを更新する間隔
+update_target_epsode_interval = 10
+# 保存間隔
+SAVE_INTERVAL = 10
 
 maze = Maze(BOARD)
 deep_q_learn = Deep_Q_learning(maze, LOSS, HIDDEN_SIZE, LEARNING_RATE, MEMORY_SIZE)
-
-# NOTE: まだ検討中
-update_interval = 50
-save_interval = 5
-# deep_q_learn.Qmodel.load_weights(f"episode{episode}.hdf5")
 
 for episode in range(EPISODE_MAX):
     step = 0
     deep_q_learn.from_start()
     # ランダムに最善でない行動を取る
-    random_rate = 0.1 + 0.9 / (1 + episode)
-    # random_rate = 0.1 + 0.95 ** (episode)
-    deep_q_learn.Qmain_to_Qtarget()
+    random_rate = max(0.98 ** episode, 0.1)
+    if episode % update_target_epsode_interval == 0:
+        deep_q_learn.Qmain_to_Qtarget()
     while not maze.is_goal() and step < STEP_MAX:
         # エージェントの1ステップ(行動、評価値の更新)
-        deep_q_learn.step(DISCOUNT_RATE, random_rate, BATCH_SIZE)
+        deep_q_learn.step(random_rate)
         # 迷路描画
         maze.draw()
-        step += 1
-        # time.sleep(SLEEP_TIME)
-
         # Qネットワークの重みを更新する
-        if step % update_interval == 0:
-            deep_q_learn.update(BATCH_SIZE, DISCOUNT_RATE)
-    # Qネットワークの重みを更新する
-    # deep_q_learn.update(BATCH_SIZE, DISCOUNT_RATE)
-    # if episode % save_interval == 0:
-    #     deep_q_learn.Qmodel.save_weights(f"episode{episode}.hdf5")
+        if step % UPDATE_INTERVAL == 0:
+            deep_q_learn.update(BATCH_SIZE, DISCOUNT_RATE, EPOCH)
+        step += 1
+        time.sleep(SLEEP_TIME)
+
+    if episode % SAVE_INTERVAL == 0:
+        deep_q_learn.save_weights(f"model{episode}.h5")
     print("\x1b[K")  # 行末までをクリア
     print(f"episode : {episode} step : {step} ")
 
